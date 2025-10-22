46af46dfd2306f34b5ec8d55f780fdaa8ef65244
FSE <amakeevmindraycom@yandex.ru>
2025-10-22 13:15:20 +0300

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,33 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Ваш ID " + id);
+        System.out.println("Ваше Имя: " + name);
+        System.out.println("=========================");
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item(1, "Ted");
+        Item item2 = new Item(2, "Bill");
+        Item item3 = new Item();
+        item1.printInfo();
+        item2.printInfo();
+        item3.printInfo();
+    }
+
     public int getId() {
         return id;
     }
