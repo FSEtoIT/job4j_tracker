01965eabfcbc372f827b38cf3bccf394cb8e270e
FSE <amakeevmindraycom@yandex.ru>
2025-10-30 10:34:17 +0300

    8. Date. Отображение даты. [#310017 #553839]_3

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,6 +6,9 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item report = new Item();
-        System.out.println(report.getCurrentDateTime());
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+
+        String formattedDate = report.getCreated().format(formatter);
+        System.out.println(formattedDate);
     }
 }
