f2ccd598033c94e26bc66660f8c615c306214199
FSE <amakeevmindraycom@yandex.ru>
2025-10-30 17:33:57 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,10 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item report = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-        String formattedDate = report.getCreated().format(formatter);
-        System.out.println(formattedDate);
+        Item item = new Item("First created Item");
+        System.out.println(item);
     }
 }
