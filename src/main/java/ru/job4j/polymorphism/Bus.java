c9330a5e40500222cdea33dbf9f9926f05241c42
FSE <amakeevmindraycom@yandex.ru>
2025-11-06 16:01:27 +0300

    4.2. Интерфейс Input [#181092 #554301]_2

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -3,16 +3,19 @@ package ru.job4j.polymorphism;
 public class Bus implements Transport {
     @Override
     public void move() {
-
+        System.out.println(getClass().getSimpleName() + " передвигается по маршруту");
     }
 
     @Override
     public void passengers(int count) {
-
+        System.out.println(getClass().getSimpleName() + count + " сидячих мест");
     }
 
     @Override
     public int refuel(int fuel) {
-        return 0;
+        int pricePerLiter = 62;
+        int cost = fuel * pricePerLiter;
+        System.out.println(getClass().getSimpleName() + fuel + " литров расходуется на маршрут. Себестоимость маршрута " + cost + "рублей");
+        return cost;
     }
 }
