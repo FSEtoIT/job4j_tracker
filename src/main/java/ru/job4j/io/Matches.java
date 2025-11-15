57cbe3f95bd6ea9c043dc5ba78ce53a01e9c7bd0
FSE <amakeevmindraycom@yandex.ru>
2025-11-15 13:29:25 +0300

    2.1. Scanner и чтение числа из консоли. [#179518 #554280]_3

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -5,38 +5,37 @@ import java.util.Scanner;
 public class Matches {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Игра 11.");
         boolean turn = true;
         int count = 11;
+
         while (count > 0) {
             String player = turn ? "Первый игрок" : "Второй игрок";
             System.out.println("На столе осталось " + count + " спичек.");
             System.out.println(player + " введите число от 1 до 3:");
             String line = input.nextLine();
+            int matches = Integer.parseInt(line);
             boolean validMove = true;
-            int matches = 0;
-            if (line.matches("\\d+")) {
-                matches = Integer.parseInt(line);
-                if (matches < 1 || matches > 3) {
-                    System.out.println("Можно брать только от 1 до 3 спичек!");
-                    validMove = false;
-                } else if (matches > count) {
-                    System.out.println("Нельзя взять больше спичек, чем осталось!");
-                    validMove = false;
-                }
-            } else {
-                System.out.println("Ошибка: нужно ввести число!");
+
+            if (matches < 1 || matches > 3) {
+                System.out.println("Можно брать только от 1 до 3 спичек!");
+                validMove = false;
+            } else if (matches > count) {
+                System.out.println("Нельзя взять больше спичек, чем осталось!");
                 validMove = false;
             }
+
             if (validMove) {
                 count -= matches;
+
                 if (count == 0) {
                     System.out.println(player + " взял последнюю спичку!");
                     break;
                 }
-                turn = !turn;
+
+                turn = !turn; // ход переключается только при корректном ходе
             }
         }
+
         if (!turn) {
             System.out.println("\nВыиграл первый игрок!");
         } else {
