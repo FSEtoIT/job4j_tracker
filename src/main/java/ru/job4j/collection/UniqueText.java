c0f768c132630c3bcdf8cc612dec40f5e51bb60f
FSE <amakeevmindraycom@yandex.ru>
2026-02-11 17:53:47 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+
+        if (origin.length != text.length) {
+            return false;
+        }
+
+        HashSet<String> check = new HashSet<>();
+        for (String word : origin) {
+            check.add(word);
+        }
+
+        for (String word : text) {
+            if (!check.contains(word)) {
+                return false;
+            }
+        }
+
+        return result;
+    }
+}
