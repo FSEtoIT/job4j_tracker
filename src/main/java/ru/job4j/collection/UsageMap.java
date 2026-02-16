686b9a4fc71bf2eae220900ce266cad36b54ee1b
FSE <amakeevmindraycom@yandex.ru>
2026-02-16 13:50:01 +0300

    0. Map, HashMap [#212652 #558258]_2

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -8,6 +8,9 @@ public class UsageMap {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev NashGeroi!");
         map.put("amakeevmindraycom@yandex.ru", "FSE to IT");
+        map.put("amakeevmindraycom@yandex.ru", "IT to FSE");
+        map.put("StasKorobeinikov@yandex.ru", "Korobeinikov Stas Mentor!");
+        map.put("KorobeinikovStas@yandex.ru", "Mentor Korobeinikov Stas!");
 
         for (Map.Entry<String, String> entry : map.entrySet()) {
             String key = entry.getKey();
