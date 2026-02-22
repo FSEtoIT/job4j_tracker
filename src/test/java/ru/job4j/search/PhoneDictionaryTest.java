d317530206b4537822fabe6c9325924aa2dcaa83
FSE <amakeevmindraycom@yandex.ru>
2026-02-22 18:56:49 +0300

    7. Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -24,4 +24,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("sadfgvwerfg2w435t");
         assertThat(persons).isEmpty();
     }
+
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
 }
